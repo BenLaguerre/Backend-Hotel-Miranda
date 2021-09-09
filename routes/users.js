f8d61132323751a0cc1b3beae982199ba91697a2
Benoit Laguerre <be.laguerre@gmail.com>
2021-09-09 16:56:16 +0200

    Test change

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
-  res.send('did it change? we wil SEE');
+  res.send('VERSION 2 : did it change? we wil SEE');
 });
 
 module.exports = router;
